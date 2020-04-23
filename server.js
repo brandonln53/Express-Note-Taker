4379d7aa568efac932bf1f96ffdb606d852a9ab3
BrandonLN <brandonln@gmail.com>
2020-04-23 09:12:09 -0600

    final project committed

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
+const apiRoutes = require("./routes/api-routes");
+const htmlRoutes = require("./routes/html-routes");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => console.log(PORT));
+
